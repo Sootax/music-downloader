728f2677c86cf502b7aa8a67f4b04325e364b659
Sootax <3jeunami82lekw@gmail.com>
2023-05-26 10:25:30 +0200

    Change validChannels

--- src/preload.js
+++ src/preload.js
@@ -4,13 +4,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
   send: (channel, data) => {
-    const validChannels = ['download', 'selectPath', 'openGithub', 'saveSettings', 'getSettings', 'componentReady'];
+    const validChannels = ['download', 'selectPath', 'error', 'openGithub', 'saveSettings', 'getSettings', 'componentReady'];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     }
   },
   receive: (channel, func) => {
-    const validChannels = ['download', 'progress', 'getSettings', 'getCurrentSong'];
+    const validChannels = ['download', 'progress', 'error', 'getSettings', 'getCurrentSong'];
     if (validChannels.includes(channel)) {
       ipcRenderer.on(channel, (event, ...args) => func(...args));
     }
